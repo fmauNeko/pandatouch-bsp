10b61b7006e44be5ad2f5af53dda7c76f50dc3c6
Florian Maunier <fmauneko@dissidence.ovh>
2026-02-20 08:56:31 +0100

    feat(touch): implement I2C bus and GT911 touch driver

--- pandatouch/src/bsp_touch.c
+++ pandatouch/src/bsp_touch.c
@@ -3,8 +3,13 @@
  *
  * SPDX-License-Identifier: MIT
  */
+#include "driver/i2c_master.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_touch_gt911.h"
+#include "esp_log.h"
 #include "bsp/pandatouch.h"
 #include "bsp/touch.h"
+#include "bsp/display.h"
 #include "bsp_err_check.h"
 
 static i2c_master_bus_handle_t i2c_handle = NULL;
@@ -15,12 +20,23 @@ esp_err_t bsp_i2c_init(void)
     if (i2c_initialized) {
         return ESP_OK;
     }
+    i2c_master_bus_config_t i2c_config = {
+        .i2c_port = CONFIG_BSP_I2C_NUM,
+        .sda_io_num = BSP_I2C_SDA,
+        .scl_io_num = BSP_I2C_SCL,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    BSP_ERROR_CHECK_RETURN_ERR(i2c_new_master_bus(&i2c_config, &i2c_handle));
     i2c_initialized = true;
     return ESP_OK;
 }
 
 esp_err_t bsp_i2c_deinit(void)
 {
+    BSP_ERROR_CHECK_RETURN_ERR(i2c_del_master_bus(i2c_handle));
+    i2c_handle = NULL;
     i2c_initialized = false;
     return ESP_OK;
 }
@@ -34,5 +50,28 @@ i2c_master_bus_handle_t bsp_i2c_get_handle(void)
 esp_err_t bsp_touch_new(const bsp_touch_config_t *config,
                          esp_lcd_touch_handle_t   *ret_touch)
 {
+    BSP_NULL_CHECK(ret_touch, ESP_ERR_INVALID_ARG);
+    BSP_ERROR_CHECK_RETURN_ERR(bsp_i2c_init());
+
+    esp_lcd_panel_io_handle_t tp_io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_GT911_CONFIG();
+    BSP_ERROR_CHECK_RETURN_ERR(esp_lcd_new_panel_io_i2c(i2c_handle, &tp_io_config, &tp_io_handle));
+
+    esp_lcd_touch_config_t tp_cfg = {
+        .x_max = BSP_LCD_H_RES,
+        .y_max = BSP_LCD_V_RES,
+        .rst_gpio_num = BSP_TOUCH_RST,
+        .int_gpio_num = BSP_TOUCH_INT,
+        .levels = {
+            .reset = 0,
+            .interrupt = 0,
+        },
+        .flags = {
+            .swap_xy = 0,
+            .mirror_x = 0,
+            .mirror_y = 0,
+        },
+    };
+    BSP_ERROR_CHECK_RETURN_ERR(esp_lcd_touch_new_i2c_gt911(tp_io_handle, &tp_cfg, ret_touch));
     return ESP_OK;
 }
